97605f32f546522bb4261de61b3881fb606b7d6c
Med Reda Kamal <kamencybusiness@gmail.com>
2021-12-09 00:31:59 +0100

    basic html & adding font

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
 <?php require_once "./inc/head.php"; ?>
+
+<div class="kv-container">
+    <h2>KamVCard</h2>
+    <p>Export your personal & professional files into a vCard file (.vcf)</p>
+
+    <form class="kv-form" method="post">
+       <p>Fill in & Export</p>
+    </form>
+</div>
