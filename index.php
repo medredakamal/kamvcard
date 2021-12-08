c75a206ba82f6ec51bdbbad13ad68764636e843f
Med Reda Kamal <kamencybusiness@gmail.com>
2021-12-09 00:42:35 +0100

    adding form fields + submit button

--- index.php
+++ index.php
@@ -1,10 +1,45 @@
-<?php require_once "./inc/head.php"; ?>
+<?php require_once './inc/head.php';
+?>
 
-<div class="kv-container">
+<div class='kv-container'>
     <h2>KamVCard</h2>
-    <p>Export your personal & professional files into a vCard file (.vcf)</p>
+    <p>Export your personal & professional files into a vCard file ( .vcf )</p>
 
-    <form class="kv-form" method="post">
-       <p>Fill in & Export</p>
+    <form class='kv-form' method='post'>
+        <ul>
+            <li>
+                <label>First name : </label>
+                <input type="text" name="firstname">
+            </li>
+            <li>
+                <label>Last name : </label>
+                <input type="text" name="lastname">
+            </li>
+            <li>
+                <label>Email address : </label>
+                <input type="text" name="email">
+            </li>
+            <li>
+                <label>Phone number : </label>
+                <input type="text" name="phone">
+            </li>
+            <li>
+                <label>Website (or LinkedIn Profile): </label>
+                <input type="text" name="website">
+            </li>
+            <li>
+                <label>Company : </label>
+                <input type="text" name="company">
+            </li>
+            <li>
+                <label>Office phone : </label>
+                <input type="text" name="officephone">
+            </li>
+            <li>
+                <label>Work position : </label>
+                <input type="text" name="workposition">
+            </li>
+            <button type="submit" class="btn-export">Export</button>
+        </ul>
     </form>
 </div>
