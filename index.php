063b849bd071aaaecded86709a6662040a7fcd1f
Med Reda Kamal <kamencybusiness@gmail.com>
2021-12-09 23:35:53 +0100

    Form data + debug

--- index.php
+++ index.php
@@ -1,11 +1,39 @@
-<?php require_once './inc/head.php';
+<?php
+
+# Require head file
+require_once './inc/head.php';
+
+# Display all php errors
+ini_set("display_errors", 1);
+ini_set("display_startup_errors", 1);
+error_reporting(E_ALL);
+
+# Export Post Action
+if (isset($_POST["action"]) && $_POST["action"] == "export_vcard") {
+
+    $userdata = [
+        'firstname'    => isset($_POST["firstname"]) ? $_POST["firstname"] : "",
+        'lastname'     => isset($_POST["lastname"]) ? $_POST["lastname"] : "",
+        'email'        => isset($_POST["email"]) ? $_POST["email"] : "",
+        'phone'        => isset($_POST["phone"]) ? $_POST["phone"] : "",
+        'website'      => isset($_POST["website"]) ? $_POST["website"] : "",
+        'company'      => isset($_POST["company"]) ? $_POST["company"] : "",
+        'officephone'  => isset($_POST["officephone"]) ? $_POST["officephone"] : "",
+        'workposition' => isset($_POST["workposition"]) ? $_POST["workposition"] : "",
+    ];
+
+    # Debug $userdata variable
+    var_dump($userdata);
+}
+
 ?>
 
-<div class='kv-container'>
+<div class="kv-container">
     <h2>KamVCard</h2>
     <p>Export your personal & professional profile into a vCard file ( .vcf )</p>
 
     <form class="kv-form" method="post">
+        <input type="hidden" name="action" value="export_vcard">
         <ul>
             <li>
                 <label>First name : </label><br />
